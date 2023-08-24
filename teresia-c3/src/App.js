e6181d411acfd9b05fd03b449d455905e0f3e754
Teresia <teresia.muthoni@azubiafrica.org>
2023-08-24 14:15:27 +0200

    Modified file

--- teresia-c3/src/App.js
+++ teresia-c3/src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1> My Portfolio</h1>
-      <p> Full Name: Teresia Muthoni</p>
-      <p>educational Background: Cetficate in Sotware Development</p>
-      <p>Profrssional Background: Junior Frontend Developer with less than a year of experience</p>
+      <h1>My Portfolio</h1>
+      <p>Full Name: Teresia Muthoni</p>
+      <p>Educational Background: Certficate in Sotware Development</p>
+      <p>Professional Background: Junior Frontend Developer with less than a year of experience</p>
       <p>Skills: HTML, CSS, JavaScript, React.</p>
     </div>
   );
